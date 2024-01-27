25502af6676d47d4b1037d87efc8b5918fd2948c
elphan <starsoccerethan@gmail.com>
2024-01-26 21:42:02 -0500

    Task box changes, non-function atm.

--- pengy-portal-frontend/src/app/to-do/to-do.component.ts
+++ pengy-portal-frontend/src/app/to-do/to-do.component.ts
@@ -11,15 +11,40 @@ export class ToDoComponent {
     this.idIndex = 0;
   }
   public addTask(): void{
+    
+    const add_button = this.el.nativeElement.querySelector('.task-add-button');
+    this.renderer.setStyle(add_button, "display", "none");
+
+    const body = `
+      <label>task*</label>
+      <br>
+      <input class = "user-vals" type="text" id="task" required>
+      <br>
+      <label>due date</label>
+      <br>
+      <input class = "user-vals" type="datetime-local" id="due-date">
+      <br>
+      <input class="submit" type="submit" value="add it!">
+    `;
+
     var div = this.renderer.createElement("div");
     this.renderer.addClass(div,"task-item");
     this.renderer.setProperty(div, "id", "temp-container");
-    const text = this.renderer.createText("The div is added!");
-    this.renderer.appendChild(div, text);
+    div.innerHTML = body;
+
+    const submit = this.el.nativeElement.querySelector('.submit');
+    this.renderer.listen(submit, 'click',()=>this.subClick());
     
     const existing = this.el.nativeElement.querySelector('.task-container');
     // Append the new div to the existing div
     this.renderer.appendChild(existing, div);
 
+  }
+  public subClick(): void{
+    const add_button = this.el.nativeElement.querySelector('.task-add-button');
+    this.renderer.setStyle(add_button, "display", "block");
+    
+    const temp = this.el.nativeElement.querySelector('.temp-container');
+    this.renderer.removeChild(this.renderer.parentNode(temp), temp);
   }
 }

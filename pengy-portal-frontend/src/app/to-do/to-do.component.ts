63c3f99f95e1c188b5ee7cbdb3a81ef3ab0f1bf5
elphan <starsoccerethan@gmail.com>
2024-02-26 14:29:54 -0500

    task container needs to be redone.

--- pengy-portal-frontend/src/app/to-do/to-do.component.ts
+++ pengy-portal-frontend/src/app/to-do/to-do.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { Component, ElementRef, Input, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-to-do',
@@ -15,25 +15,45 @@ export class ToDoComponent {
     const add_button = this.el.nativeElement.querySelector('.task-add-button');
     this.renderer.setStyle(add_button, "display", "none");
 
-    const body = `
-      <label>task*</label>
-      <br>
-      <input class = "user-vals" type="text" id="task" required>
-      <br>
-      <label>due date</label>
-      <br>
-      <input class = "user-vals" type="datetime-local" id="due-date">
-      <br>
-      <input class="submit" type="submit" value="add it!">
-    `;
 
     var div = this.renderer.createElement("div");
     this.renderer.addClass(div,"task-item");
     this.renderer.setProperty(div, "id", "temp-container");
-    div.innerHTML = body;
+    // div.innerHTML = body;
 
-    const submit = this.el.nativeElement.querySelector('.submit');
-    this.renderer.listen(submit, 'click',()=>this.subClick());
+    var label_task = this.renderer.createElement("label");
+    this.renderer.appendChild(label_task, this.renderer.createText("task*"))
+
+    var br1 = this.renderer.createElement("br");
+    var br2 = this.renderer.createElement("br");
+    var br3 = this.renderer.createElement("br");
+    var br4 = this.renderer.createElement("br");
+
+    var input_task = this.renderer.createElement("input");
+    this.renderer.addClass(input_task, "user-vals")
+    this.renderer.setProperty(input_task, "type", "text")
+    this.renderer.setProperty(input_task, "id", "task")
+    
+
+    var label_date = this.renderer.createElement("label");
+    this.renderer.appendChild(label_date, this.renderer.createText("due date"))
+
+    var input_date = this.renderer.createElement("input");
+    this.renderer.addClass(input_date, "user-vals")
+    this.renderer.setProperty(input_date, "type", "datetime-local")
+    this.renderer.setProperty(input_date, "id", "due-date")
+
+    var submit = this.renderer.createElement("input");
+    this.renderer.addClass(submit, "submit")
+    this.renderer.setProperty(submit, "type", "submit")
+    this.renderer.setProperty(submit, "id", "submit")
+    this.renderer.setProperty(submit, "value", "add it!")
+    this.renderer.listen(submit, "click", () => this.subClick())
+    
+    var elms = [label_task, br1, input_task, br2, label_date, br3, input_date, br4, submit]
+    for(var i =0; i < elms.length; i++){
+      this.renderer.appendChild(div, elms[i])
+    }
     
     const existing = this.el.nativeElement.querySelector('.task-container');
     // Append the new div to the existing div
@@ -44,7 +64,8 @@ export class ToDoComponent {
     const add_button = this.el.nativeElement.querySelector('.task-add-button');
     this.renderer.setStyle(add_button, "display", "block");
     
-    const temp = this.el.nativeElement.querySelector('.temp-container');
-    this.renderer.removeChild(this.renderer.parentNode(temp), temp);
+    const task = this.el.nativeElement.querySelector('.temp-container');
+    this.renderer.setStyle(task, "display", "none");
+
   }
 }

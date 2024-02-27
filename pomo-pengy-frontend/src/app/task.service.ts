9c5fb7ca0e8eae67d8f363b9de2676c9540aeb57
elphan <starsoccerethan@gmail.com>
2024-02-26 23:06:51 -0500

    functional task list using models and services

---
+++ pomo-pengy-frontend/src/app/task.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Task } from './task.model';
+
+// Service (Methods) that can can be used with the task model (interface) we defined.
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private tasks: Task[] = [];
+
+  getTasks(): Task[] {
+    return this.tasks;
+  }
+
+  addTask(task: Task): void {
+    this.tasks.push(task);
+  }
+
+  removeTask(index: number): void {
+    this.tasks.splice(index, 1);
+  }
+}
--- pomo-pengy-frontend/src/app/to-do/to-do.component.ts
+++ pomo-pengy-frontend/src/app/to-do/to-do.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, Input, Renderer2 } from '@angular/core';
+import { Task } from '../task.model';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-to-do',
@@ -6,66 +8,33 @@ import { Component, ElementRef, Input, Renderer2 } from '@angular/core';
   styleUrls: ['./to-do.component.scss']
 })
 export class ToDoComponent {
-  private idIndex:number;
-  constructor(private el: ElementRef, private renderer: Renderer2){
-    this.idIndex = 0;
+  tasks: Task[] = [];
+  newTask: Task = { title: '', dueDate: new Date() };
+
+  constructor(private taskService: TaskService, private el: ElementRef, private renderer: Renderer2) {
+    this.tasks = taskService.getTasks();
   }
-  public addTask(): void{
-    
-    const add_button = this.el.nativeElement.querySelector('.task-add-button');
-    this.renderer.setStyle(add_button, "display", "none");
 
+  addTask(): void {
+    this.taskService.addTask(this.newTask);
+    this.newTask = { title: '', dueDate: new Date() };
 
-    var div = this.renderer.createElement("div");
-    this.renderer.addClass(div,"task-item");
-    this.renderer.setProperty(div, "id", "temp-container");
-    // div.innerHTML = body;
+    const container = this.el.nativeElement.querySelector('.temp-container');
+    this.renderer.setStyle(container, "display", "none");
 
-    var label_task = this.renderer.createElement("label");
-    this.renderer.appendChild(label_task, this.renderer.createText("task*"))
-
-    var br1 = this.renderer.createElement("br");
-    var br2 = this.renderer.createElement("br");
-    var br3 = this.renderer.createElement("br");
-    var br4 = this.renderer.createElement("br");
-
-    var input_task = this.renderer.createElement("input");
-    this.renderer.addClass(input_task, "user-vals")
-    this.renderer.setProperty(input_task, "type", "text")
-    this.renderer.setProperty(input_task, "id", "task")
-    
-
-    var label_date = this.renderer.createElement("label");
-    this.renderer.appendChild(label_date, this.renderer.createText("due date"))
-
-    var input_date = this.renderer.createElement("input");
-    this.renderer.addClass(input_date, "user-vals")
-    this.renderer.setProperty(input_date, "type", "datetime-local")
-    this.renderer.setProperty(input_date, "id", "due-date")
-
-    var submit = this.renderer.createElement("input");
-    this.renderer.addClass(submit, "submit")
-    this.renderer.setProperty(submit, "type", "submit")
-    this.renderer.setProperty(submit, "id", "submit")
-    this.renderer.setProperty(submit, "value", "add it!")
-    this.renderer.listen(submit, "click", () => this.subClick())
-    
-    var elms = [label_task, br1, input_task, br2, label_date, br3, input_date, br4, submit]
-    for(var i =0; i < elms.length; i++){
-      this.renderer.appendChild(div, elms[i])
-    }
-    
-    const existing = this.el.nativeElement.querySelector('.task-container');
-    // Append the new div to the existing div
-    this.renderer.appendChild(existing, div);
-
-  }
-  public subClick(): void{
     const add_button = this.el.nativeElement.querySelector('.task-add-button');
     this.renderer.setStyle(add_button, "display", "block");
-    
-    const task = this.el.nativeElement.querySelector('.temp-container');
-    this.renderer.setStyle(task, "display", "none");
+  }
 
+  removeTask(index: number): void {
+    this.taskService.removeTask(index);
+  }
+
+  displayForm(): void{
+    const container = this.el.nativeElement.querySelector('.temp-container');
+    this.renderer.setStyle(container, "display", "block");
+
+    const add_button = this.el.nativeElement.querySelector('.task-add-button');
+    this.renderer.setStyle(add_button, "display", "none");
   }
 }
